bccc70059ed9482392eea003a6354b520b14b202
= <=>
2023-12-27 22:53:33 +0900

    2023.12.27 Commit

---
+++ DTLNSampleApp/DTLNSampleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DTLNSampleAppApp.swift
+//  DTLNSampleApp
+//
+//  Created by 홍희표 on 2023/12/27.
+//
+
+import SwiftUI
+
+@main
+struct DTLNSampleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
